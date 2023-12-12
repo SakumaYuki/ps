6a27c118ed8edd13719d3286d8c6c24149ace032
SakumaYuki <95071829+SakumaYuki@users.noreply.github.com>
2023-12-12 13:21:34 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template
 
+app = Flask(__name__, static_folder='./templates/image')
+
 app = Flask(__name__)
 
 #「/」へアクセスがあった場合に、"Hello World"の文字列を返す
@@ -8,9 +10,24 @@ def hello():
     return "Hello World"
 
 #「/templates」へアクセスがあった場合に、index.htmlを返す
-@app.route("/templates")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route('/menu')
+def menu_page():
+  return render_template('menu.html')
+
+@app.route('/another_page')
+def another_page():
+  return render_template('fake-sns.html')
+
+
+
+
+
+
+
 
 #app.pyをターミナルから直接呼び出した時だけ、app.run()を実行する
 if __name__ == "__main__":
