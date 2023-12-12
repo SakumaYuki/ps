83c7592d1f38428b841ba16c991dc388acfed0ed
SakumaYuki <95071829+SakumaYuki@users.noreply.github.com>
2023-12-12 15:34:59 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__, static_folder='./image')
 app = Flask(__name__, static_folder="./image/")
+app = Flask(__name__, static_folder='./templates/images')
 
 
 
